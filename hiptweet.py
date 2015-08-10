1fc195212cce3c720b18d6a35f2a8e2c60e205cc
David Baumgold <david@davidbaumgold.com>
2015-08-10 19:29:23 -0400

    Added send_notification scope

--- hiptweet.py
+++ hiptweet.py
@@ -15,6 +15,7 @@ hipchat_bp = OAuth2ConsumerBlueprint("hipchat", __name__,
     authorization_url="https://www.hipchat.com/users/authorize",
     token_url="https://api.hipchat.com/v2/oauth/token",
     base_url="https://api.hipchat.com/v2",
+    scopes=["send_notification"],
 )
 hipchat_bp.from_config["client_id"] = "HIPCHAT_OAUTH_CLIENT_ID"
 hipchat_bp.from_config["client_secret"] = "HIPCHAT_OAUTH_CLIENT_SECRET"
